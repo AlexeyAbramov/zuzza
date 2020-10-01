024382a35942bf3e8342dc049297a72031d90ceb
AlexeyAbramov <abramov.alexey.93@ya.ru>
2020-10-01 20:36:53 +0300

    completed feedback

--- src/script/script.js
+++ src/script/script.js
@@ -7,4 +7,11 @@ $(document).ready(function () {
     nextArrow: '<div class="arrow arrow_left"></div>',
     prevArrow: '<div class="arrow arrow_right"></div>',
   });
+  $(".list-feedback").slick({
+    slidesToShow: 3,
+    infinite: true,
+    pauseOnFocus: false,   
+    prevArrow: '<div class="feedback__arrow arrow_left"></div>',
+    nextArrow: '<div class="feedback__arrow arrow_right"></div>',
+  });
 });
