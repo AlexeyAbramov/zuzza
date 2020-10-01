8b7fbeb894e0e1ad12efb5c8b6877646135bc214
AlexeyAbramov <abramov.alexey.93@ya.ru>
2020-10-01 13:05:14 +0300

    1

---
+++ src/script/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".header-slider").slick({
+    infinite: true,
+    fade: true,
+    autoplaySpeed: 2500,
+    pauseOnFocus: false,
+    nextArrow: '<div class="arrow arrow_left"></div>',
+    prevArrow: '<div class="arrow arrow_right"></div>',
+  });
+});
